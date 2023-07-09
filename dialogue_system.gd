637e06d3e07be93fe50af346452fd0201397f106
Yohan27x <yoyo1999sb@gmail.com>
2023-07-09 04:49:42 +0200

    push

--- dialogue_system.gd
+++ dialogue_system.gd
@@ -4,10 +4,9 @@ class_name Dialogue_system
 
 #var dialogue_text = [["yohan","test test test test test \ntest test test", 3, 0.02, 0], ["liz","deux \ndeux deux ",2 ,0.02 ,2],["ana","trois\ntrois trois ", 2,1,0]]
 var dialogue_text = [
-	{"speaker" : "yohan", "text" : "dfghdfgh test sdf qsdf qsdfds ", "dialogue_speed" : 0.001, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 1, "go_to_next" : true, "text_start_delay" : 7 },
-	{"speaker" : "ana", "text" : "ouaf oauf oauf  ", "dialogue_speed" : 0.001, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 2, "go_to_next" : true, "text_start_delay" : 0.01 },
-	{"speaker" : "liz", "text" : "bababab\nbobo ", "dialogue_speed" : 0.001, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 1, "go_to_next" : true, "text_start_delay" : 0.01 },
-#	{"speaker" : "liz", "text" : "rtyertyertyerty ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 3, "go_to_next" : true },
+	{"speaker" : "yohan", "emotion" : "happy" , "text" : "bqsfdqsfqzerbo ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 0.5, "go_to_next" : true, "text_start_delay" : 7 },
+	{"speaker" : "ana", "emotion" : "" , "text" : "bqsfdqsfqzerbo ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 0.5, "go_to_next" : true, "text_start_delay" : 0.01 },
+	
 ]
 
 var dialogue_index = 0
--- dialogue_system_3.gd
+++ dialogue_system_3.gd
@@ -2,10 +2,7 @@ extends Resource
 class_name DialogueSystem3
 
 var dialogue_text = [
-	{"speaker" : "yohan", "text" : "we are in teh game !!! ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 1, "go_to_next" : true, "text_start_delay" : 0.5  },
-	{"speaker" : "ana", "text" : "ouaf oauf oauf  ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.4, "ready" : false, "stay_on_screen" : 2, "go_to_next" : true, "text_start_delay" : 0.01 },
-	{"speaker" : "liz", "text" : "bababab\nbobo ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 3, "go_to_next" : true, "text_start_delay" : 0.01 },
-#	{"speaker" : "liz", "text" : "rtyertyertyerty ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 3, "go_to_next" : true },
+	{"speaker" : "yohan", "emotion" : "happy" , "text" : "C'est la fin enfin bordel ", "dialogue_speed" : 0.01, "delay_after_trigger" : 0.001, "ready" : false, "stay_on_screen" : 0.5, "go_to_next" : true, "text_start_delay" : 0.01 },
 ]
 
 var dialogue_index = 0
